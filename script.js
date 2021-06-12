10034f17d098578944987194f22dfcca9360eb31
saitemu_issa <saitemu.issa@student.moringaschool.com>
2021-06-12 17:04:46 +0300

    trial js

--- script.js
+++ script.js
@@ -2,24 +2,25 @@ var cc = parseInt(document.getElementById("id-3"));
 var yy = parseInt(document.getElementById("id-4"));
 var mm = parseInt(document.getElementById("id-2"));
 var dd = parseInt(document.getElementById("id-1"));
-var gender = toLowercase(document.getElementById("id-5"));
+var gender = document.getElementById("id-5");
 
 var maleAkanaNames = ["Kwasi", "Kwadwo", "Kwabena", "Kwaku", " Yaw", "kofi", "Kwame"]
 
 var femaleAkanaNames = ["akosua", "Adwoa", "Abenaa", "akua", "yaa", "afua", "ama"]
 
 function calculate() {
-    if (gender == male) {
-        let yourDayOfWeek = (((cc / 4) - 2 * cc - 1) + ((5 * yy / 4)) + ((26 * (mm + 1) / 10)) + dd) % 7
-        let element = document.getElementById("akan")
-        
-        if (yourDayOfWeek === 6) {
-            element.innerHTML ="your akana name is" + maleAkanaNames[5]
+    console.log("your akan name is")
+    if (gender == "male") {
 
-        }
+        let yourDayOfWeek = (((cc / 4) - 2 * cc - 1) + ((5 * yy / 4)) + ((26 * (mm + 1) / 10)) + dd) % 7
+        let dOw = document.getElementById("akan")
+        dOw.document.getElementById("akan").innerHTML="yourDayOfWeek"
+        console.log("your day of week" + yourDayOfWeek)
+    }
+    else{
+        console.log ("you are female")
     }
     // console.log("you proceed")
 
 
 }
-calculate()
