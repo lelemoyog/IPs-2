f9f7858aaaf7f4c2f84b6b720e6ee92aeaf59030
saitemu-issa <issasaitemu@gmail.com>
2021-06-12 16:12:11 +0300

    added data collection in js

--- script.js
+++ script.js
@@ -1,25 +1,14 @@
-var cc = parseInt(document.getElementById("id-3"));
-var yy = parseInt(document.getElementById("id-4"));
-var mm = parseInt(document.getElementById("id-2"));
-var dd = parseInt(document.getElementById("id-1"));
-var gender = toLowercase(document.getElementById("id-5"));
+var dd= document.getElementById("id-1").value;
+var mm= document.getElementById("id-2").value;
+var cc= document.getElementById("id-3").value;
+var yy= document.getElementById("id-4").value;
+var cc= document.getElementById("id-5").value;
 
-var maleAkanaNames = ["Kwasi", "Kwadwo", "Kwabena", "Kwaku", " Yaw", "kofi", "Kwame"]
-
-var femaleAkanaNames = ["akosua", "Adwoa", "Abenaa", "akua", "yaa", "afua", "ama"]
-
-function calculate() {
-    if (gender == male) {
-        let yourDayOfWeek = (((cc / 4) - 2 * cc - 1) + ((5 * yy / 4)) + ((26 * (mm + 1) / 10)) + dd) % 7
-        let element = document.getElementById("akan")
-        
-        if (yourDayOfWeek === 6) {
-            element.innerHTML ="your akana name is" + maleAkanaNames[5]
-
-        }
-    }
-    // console.log("you proceed")
+const form = document.querySelector('form');
+const data = new FormData(form);
 
 
+function submit(){
+    e.preventDefault();
+    console.log(data)
 }
-calculate()
